6a01567415a1f619a94e02920221b62edb14bc90
ntheile <ntheile@gmail.com>
2023-09-08 10:20:54 -0400

    add webln

--- src/app/components/Inventory.tsx
+++ src/app/components/Inventory.tsx
@@ -1,6 +1,7 @@
 
 "use client"
 import * as React from "react";
+import { requestProvider } from "webln";
 
 const Inventory: React.FunctionComponent = () => {
   const [showModalBuy, setShowModalBuy] = React.useState(false);
@@ -10,6 +11,22 @@ const Inventory: React.FunctionComponent = () => {
     price: 1.1,
     item: "1.0 BTC",
   });
+
+  const submit = async () =>{
+    signMessage(`Buying ${cartItem.name}`)
+    setShowModalBuy(false)
+  }
+
+  const signMessage = async (msg: string) =>{
+    try {
+      const webln = await requestProvider()
+      const resp = await webln.signMessage(msg)
+      const { message, signature } = resp
+      console.log("Signed message", message, signature)
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
 
   return (
     <section className="inventory py-5" id="inventory">
@@ -361,8 +378,7 @@ const Inventory: React.FunctionComponent = () => {
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModalBuy(false)}
-                  >
+                    onClick={() => submit()}>
                     Submit
                   </button>
                 </div>
